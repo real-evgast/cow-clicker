d46fa32611bf322eb1928dca3a980e82be824560
evgast <@gmail.com>
2025-01-18 15:08:49 +0300

    version 1.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# Cow clicker
+___
+## Description:
++ This is a game in which you need to milk a cow and get money for it and then upgrade the cow and the amount of money for each click.
++ All the most modern algorithms and design solutions that you will like are used here.
++ Also, during the game, a syringe may appear that increases income by 2 times.
+
+## How to start playing
+1. go to itch.io and download the archive
+2. then unzip all the contents of the archive to any folder
+3. and run the .exe file
+    + Antivirus may complain about this file because: unknown publisher. I don't know how to fix it yet. I will fix it in future versions.
+    + But you can always study my written [Code](main.py)
+___
+## Developers
+coder: evgast :kissing_smiling_eyes:
+
+designer: konvlax :smiley:
+
+tester: Vanya :zzz:
+
+## Words from the developers
+We are glad that we made a game, even such a game. This is our first game, and we will be glad for any support from your side. 
+
+The idea of this game was created in 5 minutes. This is our first step into the software community. We do not expect anything from this game. It will just be, like many similar games.
---
+++ main.py
@@ -0,0 +1,221 @@
+import pygame
+import random
+import json
+from cryptography.fernet import Fernet
+
+clock = pygame.time.Clock()
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 800))
+
+key = "jAc607-Du0yJUVqdxXNkrP1x8NDxAcb_MOClmwL6Pzw="
+cipher_suite = Fernet(key)
+
+with open('saves/save.enc', 'rb') as enc_file:
+    cipher_text = enc_file.read()
+
+json_data_bytes = cipher_suite.decrypt(cipher_text)
+save = json.loads(json_data_bytes.decode('utf-8'))
+
+# with open('saves/save.json', 'w') as json_file:
+#     json.dump(json_data, json_file, ensure_ascii = False, indent = 4)
+
+# with open('saves/save.json', 'r') as file:
+#    save = json.load(file)
+
+string_level = save['level']
+string_money = save['money']
+string_click = save['click']
+level = int(string_level)
+money = int(string_money)
+click = int(string_click)
+
+icon = pygame.image.load('image/icon.png')
+
+level_1 = pygame.image.load('image/level_1.png')
+level_2 = pygame.image.load('image/level_2.png')
+level_3 = pygame.image.load('image/level_3.png')
+level_4 = pygame.image.load('image/level_4.png')
+level_5 = pygame.image.load('image/level_5.png')
+level_6 = pygame.image.load('image/level_6.png')
+powerful = pygame.image.load('image/powerful.png')
+
+shop = pygame.image.load('image/shop.png')
+board_money = pygame.image.load('image/board_money.png')
+click_lvl_up = pygame.image.load('image/click_lvl_up.png')
+cow_lvl_ip = pygame.image.load('image/cow_lvl_up.png')
+syringe_ = pygame.image.load('image/syringe.png')
+info = pygame.image.load('image/info.png')
+
+background = pygame.image.load('image/фон.png')
+pygame.display.set_icon(icon)
+pygame.display.set_caption("cow clicker")
+
+prices_cow = [200, 500, 1000, 2000, 5000]
+
+prices_click = [50, 100,  300,  500, 1000,  3000,  5000,  7000,  10000,  15000] # 1,5; 2; 2,5; 3; 4; 5; 7; 8; 9; 15
+clicks =       [1,    2,    3,    4,    5,     7,     8,     9,     10,     15]
+
+font_constructor = pygame.font.Font('font/Jura-Medium.ttf', 60)
+
+font_info = pygame.font.Font('font/Jura-Medium.ttf', 40)
+
+font_money = font_constructor.render(str(money), False, 'white')
+font_cow = font_constructor.render(str(prices_cow[level - 1]), False, 'white')
+font_click = font_constructor.render(str(prices_click[click - 1]), False, 'white')
+
+info_level = font_info.render("lvl cow = " + str(level), False, 'white')
+info_click = font_info.render("lvl click = " + str(click), False, 'white')
+syringe = 0
+secret = 0
+def int_level():
+    global level, what_level
+    if level == 1:
+        what_level = level_1
+
+    elif level == 2:
+        what_level = level_2
+
+    elif level == 3:
+        what_level = level_3
+
+    elif level == 4:
+        what_level = level_4
+
+    elif level == 5:
+        what_level = level_5
+
+    else:
+        what_level = level_1
+        level = 1
+
+    if secret == 6:
+        what_level = level_6
+        level = 6
+
+    if syringe == 2:
+        what_level = powerful
+
+running = True
+int_level()
+
+auto_lvl_up_ = 0
+
+cor = [0, 0]
+def randomizer():
+    global syringe
+    randoms = random.randint(-30, 10)
+    if randoms > 0 and syringe == 0:
+        syringe = 1
+        cor[0] = random.randint(300, 600)
+        cor[1] = random.randint(500, 800)
+    else:
+        syringe = 0
+
+tick = 0
+
+if level == 5:
+    font_cow = font_constructor.render("", False, 'white')
+
+if click == 10:
+    font_click = font_constructor.render("", False, 'white')
+
+while running:
+    clock.tick(60)
+    tick += 1
+    if tick == 3600 and syringe == 0:
+        randomizer()
+        tick = 0
+
+    elif tick == 1800 and syringe == 2:
+        syringe = 0
+        tick = 0
+        int_level()
+        info_click = font_info.render("lvl click = " + str(click), False, 'white')
+
+    screen.blit(background, (0, 0))
+    screen.blit(what_level, (0, 0))
+    screen.blit(info, (270, -25))
+
+    screen.blit(board_money, (670, 0))
+    screen.blit(shop, (-20, 230))
+    screen.blit(cow_lvl_ip, (20, 440))
+    screen.blit(click_lvl_up, (20, 590))
+
+    screen.blit(font_money, (800, 26))
+    screen.blit(font_cow, (330, 470))
+    screen.blit(font_click, (330, 620))
+
+    screen.blit(info_level, (340, 80))
+    screen.blit(info_click, (340, 120))
+
+    if syringe == 1:
+        screen.blit(syringe_, (cor[0], cor[1]))
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            pos = event.pos
+            if 0 < pos[0] < 300 and pos[1] < 300:
+                if syringe == 2:
+                    money += clicks[click - 1] * 2
+
+                else:
+                    money += clicks[click - 1]
+
+                font_money = font_constructor.render(str(money), False, 'white')
+
+            elif 20 < pos[0] < 306 and 440 < pos[1] < 570:
+                if money > prices_cow[level - 1] or money == prices_cow[level - 1]:
+                    if level < 5:
+                        money = money - prices_cow[level - 1]
+                        font_money = font_constructor.render(str(money), False, 'white')
+                        level += 1
+                        font_cow = font_constructor.render(str(prices_cow[level - 1]), False, 'white')
+                        info_level = font_info.render("lvl cow = " + str(level), False, 'white')
+                        int_level()
+
+                elif level == 5:
+                    font_cow = font_constructor.render("", False, 'white')
+                    secret += 1
+                    print(secret)
+                    int_level()
+
+            elif 20 < pos[0] < 316 and 590 < pos[1] < 708:
+                if prices_click[click - 1] < money or prices_click[click - 1] == money:
+                    if click < 10:
+                        money = money - prices_click[click - 1]
+                        click += 1
+                        font_money = font_constructor.render(str(money), False, 'white')
+                        font_click = font_constructor.render(str(prices_click[click - 1]), False, 'white')
+                        info_click = font_info.render("lvl click = " + str(click), False, 'white')
+
+                    if click == 10:
+                        font_click = font_constructor.render("", False, 'white')
+
+
+            elif 20 < pos[0] < 466 and 740 < pos[1] < 858:
+                auto_lvl_up_ = 1
+
+            elif cor[0] < pos[0] < cor[0] + 130 and cor[1] < pos[1] < cor[1] + 130 and syringe == 1:
+                syringe = 2
+                int_level()
+                info_click = font_info.render("lvl click = " + str(click) + " x 2!!!", False, 'white')
+
+
+
+save['level'] = str(level)
+save['money'] = str(money)
+save['click'] = str(click)
+# with open('saves/save.json', 'w') as file:
+#     json.dump(save, file, ensure_ascii = False, indent = 4)
+
+save_enc = json.dumps(save).encode('utf-8')
+cipher_text = cipher_suite.encrypt(save_enc)
+
+with open('saves/save.enc', 'wb') as enc_file:
+    enc_file.write(cipher_text)
